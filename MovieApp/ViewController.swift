378a84a6a292bec7fedafed2fd6f29c6421f28ab
Rival Fauzi <58524125+rivalku21@users.noreply.github.com>
2025-05-25 08:13:24 +0700

    first commit

---
+++ MovieApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by Rival Fauzi on 25/05/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MovieAppTests/MovieAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppTests.swift
+//  MovieAppTests
+//
+//  Created by Rival Fauzi on 25/05/25.
+//
+
+import Testing
+@testable import MovieApp
+
+struct MovieAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
